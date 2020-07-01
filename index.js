b4ed4a6f366d4b72b6a9a2a0f630a646e8aeee45
hark17 <harjeevs17@gmail.com>
2020-07-02 00:17:37 +0530

    Deletion

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.set('port', (process.env.PORT || 8080))
+app.set("port", process.env.PORT || 8080);
 app.use(express.static(__dirname + "/public"));
 
 const url = "mongodb://localhost:27017/addition";
@@ -39,18 +39,23 @@ app.post("/insert-movie", function (req, res, next) {
         rating: req.body.rating,
 		id:req.body.id,
       })*/
-	  watchs.findOneAndUpdate({id:req.body.id},{$set:
-	  {
-		title: req.body.title,
-        f_image: req.body.f_image,
-        b_image: req.body.b_image,
-        description: req.body.description,
-        status: req.body.status,
-        rating: req.body.rating,
-		id:req.body.id,
-		review:req.body.review,
-		}
-		},{upsert:true})
+    watchs
+      .findOneAndUpdate(
+        { id: req.body.id },
+        {
+          $set: {
+            title: req.body.title,
+            f_image: req.body.f_image,
+            b_image: req.body.b_image,
+            description: req.body.description,
+            status: req.body.status,
+            rating: req.body.rating,
+            id: req.body.id,
+            review: req.body.review,
+          },
+        },
+        { upsert: true }
+      )
       .then(() => {
         res.sendStatus(200);
       })
@@ -73,18 +78,22 @@ app.post("/insert-tv", function (req, res, next) {
       rating: req.body.rating,
 	  id:req.body.id,
     })*/
-	tv.findOneAndUpdate({id:req.body.id},{$set:
-	  {
-		title: req.body.title,
-        f_image: req.body.f_image,
-        b_image: req.body.b_image,
-        description: req.body.description,
-        status: req.body.status,
-        rating: req.body.rating,
-		id:req.body.id,
-		review:req.body.review,
-		}
-		},{upsert:true})
+    tv.findOneAndUpdate(
+      { id: req.body.id },
+      {
+        $set: {
+          title: req.body.title,
+          f_image: req.body.f_image,
+          b_image: req.body.b_image,
+          description: req.body.description,
+          status: req.body.status,
+          rating: req.body.rating,
+          id: req.body.id,
+          review: req.body.review,
+        },
+      },
+      { upsert: true }
+    )
       .then(() => {
         res.sendStatus(200);
       })
@@ -98,8 +107,8 @@ app.post("/insert-tv", function (req, res, next) {
 app.post("/insert-book", function (req, res, next) {
   connect.then((db) => {
     console.log("Successfully connected");
-    book
-      /*.create({
+    book;
+    /*.create({
         title: req.body.title,
         f_image: req.body.f_image,
         b_image: req.body.b_image,
@@ -108,18 +117,23 @@ app.post("/insert-book", function (req, res, next) {
         rating: req.body.rating,
 		id:req.body.id,
       })*/
-	  book.findOneAndUpdate({id:req.body.id},{$set:
-	  {
-		title: req.body.title,
-        f_image: req.body.f_image,
-        b_image: req.body.b_image,
-        description: req.body.description,
-        status: req.body.status,
-        rating: req.body.rating,
-		id:req.body.id,
-		review:req.body.review,
-		}
-		},{upsert:true})
+    book
+      .findOneAndUpdate(
+        { id: req.body.id },
+        {
+          $set: {
+            title: req.body.title,
+            f_image: req.body.f_image,
+            b_image: req.body.b_image,
+            description: req.body.description,
+            status: req.body.status,
+            rating: req.body.rating,
+            id: req.body.id,
+            review: req.body.review,
+          },
+        },
+        { upsert: true }
+      )
       .then(() => {
         res.sendStatus(200);
       })
@@ -135,8 +149,8 @@ app.get("/fetch-movies/:status", function (req, res, next) {
   connect.then((db) => {
     console.log("Connected successfully");
     watchs
-      .find({status:req.params.status})
-      .sort({ status: -1 })
+      .find({ status: req.params.status })
+      .sort({ createdAt: -1 })
       .then((result) => {
         res.send(result);
       })
@@ -149,8 +163,8 @@ app.get("/fetch-books/:status", function (req, res, next) {
   connect.then((db) => {
     console.log("Connected successfully");
     book
-      .find({status:req.params.status})
-      .sort({ status: -1 })
+      .find({ status: req.params.status })
+      .sort({ createdAt: -1 })
       .then((result) => {
         res.send(result);
       })
@@ -162,8 +176,8 @@ app.get("/fetch-books/:status", function (req, res, next) {
 app.get("/fetch-tv/:status", function (req, res, next) {
   connect.then((db) => {
     console.log("Connected successfully");
-    tv.find({status:req.params.status})
-      .sort({ status: -1 })
+    tv.find({ status: req.params.status })
+      .sort({ createdAt: -1 })
       .then((result) => {
         res.send(result);
       })
@@ -174,9 +188,10 @@ app.get("/fetch-tv/:status", function (req, res, next) {
 });
 
 app.get("/fetch-movie-rating/:id", function (req, res, next) {
-	connect.then((db) => {
+  connect.then((db) => {
     console.log("Connected successfully");
-    watchs.find({id:req.params.id}, {rating:1,review:1})
+    watchs
+      .find({ id: req.params.id }, { rating: 1, review: 1 })
       .sort({ status: -1 })
       .then((result) => {
         res.send(result);
@@ -188,9 +203,9 @@ app.get("/fetch-movie-rating/:id", function (req, res, next) {
 });
 
 app.get("/fetch-tv-rating/:id", function (req, res, next) {
-	connect.then((db) => {
+  connect.then((db) => {
     console.log("Connected successfully");
-    tv.find({id:req.params.id}, {rating:1,review:1})
+    tv.find({ id: req.params.id }, { rating: 1, review: 1 })
       .sort({ status: -1 })
       .then((result) => {
         res.send(result);
@@ -202,9 +217,10 @@ app.get("/fetch-tv-rating/:id", function (req, res, next) {
 });
 
 app.get("/fetch-book-rating/:id", function (req, res, next) {
-	connect.then((db) => {
+  connect.then((db) => {
     console.log("Connected successfully");
-    book.find({id:req.params.id}, {rating:1,review:1})
+    book
+      .find({ id: req.params.id }, { rating: 1, review: 1 })
       .sort({ status: -1 })
       .then((result) => {
         res.send(result);
@@ -214,10 +230,45 @@ app.get("/fetch-book-rating/:id", function (req, res, next) {
       });
   });
 });
+app.get("/delete-movie/:id", function (req, res) {
+  connect.then((db) => {
+    watchs.deleteOne({ id: req.params.id }, function (err, obj) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(200);
+        console.log("Item deleted");
+      }
+    });
+  });
+});
+app.get("/delete-book/:id", function (req, res) {
+  connect.then((db) => {
+    book.deleteOne({ id: req.params.id }, function (err, obj) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(200);
+        console.log("Item deleted");
+      }
+    });
+  });
+});
+app.get("/delete-tv-show/:id", function (req, res) {
+  connect.then((db) => {
+    tv.deleteOne({ id: req.params.id }, function (err, obj) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(200);
+        console.log("Item deleted");
+      }
+    });
+  });
+});
 
 const server = http.createServer(app);
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
+app.listen(app.get("port"), function () {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
